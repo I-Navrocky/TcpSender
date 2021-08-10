eca4b56cbd2c001ca7f5c0ae8e7e6232829d0c0f
Inavrocky <ivnavrotskiy@yandex.ru>
2021-08-10 10:30:50 +0300

    initial commit

--- TcpSender/Program.cs
+++ TcpSender/Program.cs
@@ -6,7 +6,7 @@ namespace TcpSender
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");    
         }
     }
 }
